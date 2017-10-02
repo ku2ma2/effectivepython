bf49fbf547174af47d819b7fa51687641a0f6f84
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-10-02 14:26:38 +0900

    Add to_str and to_bytes function in bytes_str.py

--- 03_bytes_str/bytes_str.py
+++ 03_bytes_str/bytes_str.py
@@ -9,7 +9,10 @@ def to_str(bytes_or_str):
     バイナリ(bytes)か文字列(str)かが分からない状態で bytes_or_str が
     与えられてバイナリだった場合は文字列にして返す
     """
-    return b'string'
+    if isinstance(bytes_or_str, bytes):
+        return bytes_or_str.decode('utf-8')
+
+    return bytes_or_str
 
 
 def to_bytes(bytes_or_str):
@@ -18,6 +21,9 @@ def to_bytes(bytes_or_str):
     バイナリ(bytes)か文字列(str)かが分からない状態で bytes_or_str が
     与えられてstr型だった場合はバイナリ(bytes)にして返す
     """
+    if isinstance(bytes_or_str, str):
+        return bytes_or_str.encode('utf-8')
+
     return bytes_or_str
 
 
--- 03_bytes_str/tests/test_bytes_str.py
+++ 03_bytes_str/tests/test_bytes_str.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from bytes_str import to_str
+from bytes_str import to_str, to_bytes
 
 
 class TestBytesStr(unittest.TestCase):
@@ -12,21 +12,47 @@ class TestBytesStr(unittest.TestCase):
     Stringへの変換
     """
 
-    def test_to_str_str型だった場合str型がそのまま返える(self):
+    def test_to_str_in_str(self):
         """
         str型だった場合str型がそのまま返える
         """
         actual = isinstance(to_str('string'), str)
         self.assertTrue(actual)
 
-    def test_to_str_バイナリだった場合str型に変換して返す(self):
+    def test_to_str_in_bytes(self):
         """
         バイナリだった場合str型に変換して返す
         """
-        expected = 'binary'
-        actual = isinstance(to_str(b'binary'), str)
+        expected = 'bytes'
+
+        to_str_data = to_str(b'bytes')
+        actual = isinstance(to_str_data, str)
         self.assertTrue(actual)
-        self.assertEqual(actual, expected)
+        self.assertEqual(to_str_data, expected)
+
+
+class TestStrBytes(unittest.TestCase):
+    """
+    バイナリ(bytes)への変換
+    """
+
+    def test_to_bytes_in_bytes(self):
+        """
+        バイナリだった場合そのまま返えす
+        """
+        actual = isinstance(to_bytes(b'bytes'), bytes)
+        self.assertTrue(actual)
+
+    def test_to_bytes_in_str(self):
+        """
+        バイナリだった場合str型に変換して返す
+        """
+        expected = b'binary'
+
+        to_bytes_data = to_bytes('binary')
+        actual = isinstance(to_bytes_data, bytes)
+        self.assertTrue(actual)
+        self.assertEqual(to_bytes_data, expected)
 
 
 if __name__ == '__main__':
