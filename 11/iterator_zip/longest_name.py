8dc9c8df3c303f65da660ac152d30c6c59687c7c
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-10-10 07:35:01 +0900

    Add sample zip for iterator

---
+++ 11/iterator_zip/longest_name.py
@@ -0,0 +1,31 @@
+"""
+## 項目11: イテレータを並列に処理するにはzipを使う
+
+- 組み込み関数zipが複数のイテレータを並列に処理するのに使える
+- 異なる長さのイテレータを与えると、zipは何も言わずに出力を最短で止める
+"""
+
+
+def get_longest(names: list) -> str:
+    """
+    入力されたリスト内の一番長い文字列を返す
+
+    >>> longest_name(['one','two','three'])
+    'three'
+
+    """
+
+    letters = [len(n) for n in names]
+    longest_name = None
+    max_letters = 0
+
+    for name, count in zip(names, letters):
+        if count > max_letters:
+            longest_name = name
+            max_letters = count
+
+    return longest_name
+
+
+if __name__ == "__main__":
+    print(convert_enumerate(['one', 'two', 'three']))
