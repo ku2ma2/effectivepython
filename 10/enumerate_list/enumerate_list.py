eb69972663f386398fb4cad755a1daf1ccd7366c
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-10-09 09:58:00 +0900

    Add enumerate_list.py

---
+++ 10/enumerate_list/enumerate_list.py
@@ -0,0 +1,31 @@
+"""
+## 項目10: rangeよりはenumerateにする
+
+- リストの要素の添え字を利用する事があるが、rangeを使うより enumerateの方が自然
+- enumerateはカウンタを開始する数も指定可能
+"""
+
+
+def convert_enumerate(origin: list, start: int = 0) -> list:
+    """
+    入力されたリストをインデックスを追加したタプルをリスト化して返す
+    また、カウンタを開始する数 start を指定するとその数から開始する
+
+    >>> convert_enumerate(['one','two','three'])
+    [(0,'one'), (1,'two'), (2,'three')}
+
+    >>> convert_enumerate(['one','two','three'], 1)
+    [(1,'one'), (2,'two'), (3,'three')}
+    """
+
+    result = []
+    for i, item in enumerate(origin, start):
+        result.append((i, item))
+
+    return result
+
+
+if __name__ == "__main__":
+    lists = convert_enumerate(['one', 'two', 'three'])
+    for i, item in lists:
+        print(i, item)
