3e2d6450efdca2e06552b4075cab905a96cc6097
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-10-11 21:03:25 +0900

    Finished devide_json.py

--- 13/try_except_block/devide_json.py
+++ 13/try_except_block/devide_json.py
@@ -1,13 +1,16 @@
 """
-## 項目13: try/except/else/finallyの各ブロックを活用する
+項目13: try, except,else, finally の各ブロックを活用する
 
 - try/finally 文では、tryブロックで例外が起きるかは関係なく必ず後処理を実行する
 - elseは try記述を最小にして処理を分離して可読性をあげる
 - elseは finallyの共通後処理前に追加作業をするのに便利
 """
 
+import json
+UNDEFINED = []
 
-def devide_json(names: list) -> str:
+
+def devide_json(path: str):
     """
     入力されたリスト内の一番長い文字列を返す
 
@@ -15,18 +18,28 @@ def devide_json(names: list) -> str:
     'three'
 
     """
+    handle = open(path, 'r+')
 
-    letters = [len(n) for n in names]
-    longest_name = None
-    max_letters = 0
+    try:
+        data = handle.read()
+        op = json.loads(data)
 
-    for name, count in zip(names, letters):
-        if count > max_letters:
-            longest_name = name
-            max_letters = count
+        value = op['numerator'] / op['denominator']  # ZeroDivisionErrorが出るかも
+        # とその前に op の中に何も入ってなければ　KeyErrorが出るわけだが…
 
-    return longest_name
+    except ZeroDivisionError as e:
+        return UNDEFINED
+
+    else:
+        op['result'] = value
+        result = json.dumps(op)
+        handle.seek(0)
+        handle.write(result)  # IOError 起こすかも
+        return value
+
+    finally:
+        handle.close()  # 常に最後はファイルハンドルを閉じる
 
 
 if __name__ == "__main__":
-    print(devide_json())
+    print(devide_json('test.json'))
--- 13/try_except_block/tests/test_devide_json.py
+++ 13/try_except_block/tests/test_devide_json.py
@@ -1,28 +1,76 @@
 """
-項目11: イテレータを並列に処理するにはzipを使うのテスト
+項目13: try/except/else/finallyの各ブロックを活用する
 """
 
 import sys
 import os
 import unittest
+import tempfile
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 
-from longest_name import *
+from devide_json import *
 
 
-class TestLongestName(unittest.TestCase):
+class TestDevideJson(unittest.TestCase):
     """
     リスト内の一番長い名前を取得する
     """
 
-    def test_no_default(self):
+    def test_io_error(self):
         """
-        通常パターン
+        ファイルがなかった場合はIOError
         """
 
-        actual = get_longest(['Cecilia', 'Lise', 'Marie'])
-        expected = 'Cecilia'
+        with self.assertRaises(IOError):
+            devide_json('not_found_file')
+
+    def test_value_error(self):
+        """
+        ファイルがJSON形式でなかった場合はValueError
+        """
+
+        with tempfile.NamedTemporaryFile(mode='w') as f:
+            f.write("""one
+two
+three
+""")
+            f.flush()
+            with self.assertRaises(ValueError):
+                devide_json(f.name)
+
+    def test_zero_division_error(self):
+        """
+        ファイルがJSON形式ではあるが、値が0だった場合は除算するので
+        ZeroDivisionErrorを避けて、空リストが返ってくる
+        """
+
+        with tempfile.NamedTemporaryFile(mode='w') as f:
+            f.write("""{
+    "numerator": 12,
+    "denominator": 0
+}
+""")
+            f.flush()
+            expected = []
+            actual = devide_json(f.name)
+
+        self.assertEqual(actual, expected)
+
+    def test_success(self):
+        """
+        最終的には計算した結果をrestultとして返す
+        """
+
+        with tempfile.NamedTemporaryFile(mode='w') as f:
+            f.write("""{
+    "numerator": 12,
+    "denominator": 2
+}
+""")
+            f.flush()
+            expected = 6.0
+            actual = devide_json(f.name)
 
         self.assertEqual(actual, expected)
 
