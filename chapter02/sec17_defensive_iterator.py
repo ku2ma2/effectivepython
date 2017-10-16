fcab04451839d6b946116f7c8f210d7d1149d2d4
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-10-17 07:17:27 +0900

    Add file for sec17

---
+++ chapter02/sec17_defensive_iterator.py
@@ -0,0 +1,28 @@
+"""
+項目17: 引数に対してイテレータを使うときは確実さを尊ぶ
+
+Be Defensive When Iterating Over Arguments
+
+- 日本語版書籍の翻訳に関してネガティブな意見がネットで確認したが何となくわかってきた。日本語が分かりづらい…。
+"""
+
+
+def normalize_defensive(numbers):
+    """
+    数値のリスト numbers を与えられたらその数のリスト内でのパーセンテージ
+    をリストにして返す
+
+    >>> visits = [15, 35, 80]
+    >>> normalize_defensive()
+    [0, 5, 11, 15, 21]
+    """
+    if text:
+        yield 0
+
+    for index, letter in enumerate(text):
+        if letter == ' ':
+            yield index + 1
+
+
+if __name__ == "__main__":
+    pass
