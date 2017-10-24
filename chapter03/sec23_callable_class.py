f9e29dd521b8d4909be1b9ea79d751fc9c9e4466
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-10-25 07:11:27 +0900

    Add test for section 23

--- chapter03/sec23_callable_class.py
+++ chapter03/sec23_callable_class.py
@@ -7,86 +7,24 @@ Accept Functions for Simple Interfaces Instead of Classes
 """
 
 
-class Subject(object):
+class CountMissing(object):
     """
-    科目を表すクラス
-    """
-
-    def __init__(self):
-        """
-        コンストラクタ 点数リストを初期化
-        """
-        self._grades = []
-
-    def report_grade(self, score, weight):
-        """
-        点数リストを設定する
-        """
-        self._grades.append(Grade(score, weight))
-
-    def average_grade(self):
-        """
-        科目毎に集計をするための処理
-        """
-        total, total_weight = 0, 0
-
-        for grade in self._grades:
-            total += grade.score * grade.weight
-            total_weight += grade.weight
-
-        return total / total_weight
-
-
-class Student(object):
-    """
-    生徒を表すクラス
+    内部カウントをするためのクラス
     """
 
     def __init__(self):
         """
-        コンストラクタ 科目リストを追加
+        コンストラクタ
         """
-        self._subjects = {}
+        self.added = 0
 
-    def subject(self, name):
+    def __call__(self):
         """
-        科目を追加
+        関数として呼ばれた場合の処理
+        added をインクリメントする
         """
-        if name not in self._subjects:
-            self._subjects[name] = Subject()
-        return self._subjects[name]
-
-    def average_grade(self):
-        """
-        科目の平均点を算出
-        """
-        total, count = 0, 0
-        for subject in self._subjects.values():
-            total += subject.average_grade()
-            count += 1
-
-        return total / count
-
-
-class Gradebook(object):
-    """
-    生徒の点数表
-    """
-
-    def __init__(self):
-        """
-        コンストラクタ 生徒リストを追加
-        """
-        self._students = {}
-
-    def student(self, name):
-        """
-        生徒を追加
-        """
-        if name not in self._students:
-            self._students[name] = Student()
-
-        return self._students[name]
+        self.added += 1
+        return 0
 
 
 if __name__ == "__main__":
