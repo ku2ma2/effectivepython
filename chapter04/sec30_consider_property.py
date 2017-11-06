90c90432577603b07478e576cdf14dfc714ff642
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-11-06 22:25:09 +0900

    Add function fill and deduct

--- chapter04/sec30_consider_property.py
+++ chapter04/sec30_consider_property.py
@@ -1,13 +1,16 @@
 """
-項目29: 属性をリファクタリングする代わりに@propertyを考える
+項目29: 属性をリファクタリングする代わりに property を考える
 
 Consider @property Instead of Refactoring Attributes
 
-- 
+- 例題は リーキーバケット https://ja.wikipedia.org/wiki/%E3%83%AA%E3%83%BC%E3%82%AD%E3%83%BC%E3%83%90%E3%82%B1%E3%83%83%E3%83%88
+- @propertyを使って既存のインスタンス属性に新たな機能を与える
+- @propertyを使って、より良いデータモデルへ逐次改善する
+- @propertyをあまり使いすぎるようになったら、そのクラスと全ての呼び出し元をリファクタリングする事を考える
 
 """
 
-import datetime
+from datetime import datetime, timedelta
 
 
 class Bucket(object):
@@ -19,7 +22,7 @@ class Bucket(object):
         """
         コンストラクタ
         """
-        self.period_delta = datetime.timedelta(seconds=period)
+        self.period_delta = timedelta(seconds=period)
         self.reset_time = datetime.now()
         self.quota = 0
 
@@ -30,5 +33,25 @@ class Bucket(object):
         return 'Bucket(quota={})'.format(self.quota)
 
 
+def fill(bucket, amount):
+    now = datetime.now()
+    if now - bucket.reset_time > bucket.period_delta:
+        bucket.quota = 0
+        bucket.reset_time = now
+    bucket.quota += amount
+
+
+def deduct(bucket, amount):
+    now = datetime.now()
+    if now - bucket.reset_time > bucket.period_delta:
+        return False
+    if bucket.quota - amount < 0:
+        return False
+    bucket.quota -= amount
+    return True
+
+
 if __name__ == "__main__":
-    pass
+    bucket = Bucket(period=60)
+    fill(bucket, amount=100)
+    print(bucket)
